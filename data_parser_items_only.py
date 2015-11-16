abbae0df207fd9c1eba2ca24ad197b9013d3a2c9
Gregory Enriquez <gregoryenriquez@gmail.com>
2015-11-15 23:38:03 -0800

    user id and item id mapped to ints

--- data_parser_items_only.py
+++ data_parser_items_only.py
@@ -4,39 +4,37 @@ import argparse
 import itertools
 import re
 
-class UserIdList:
+class ItemsMap:
     user_id_to_item_list = []
     file_name = ""
     writer = ""
     user_id_to_int = {}
-    user_id_dict_counter = 1
+    dict_counter = 1
     item_id_to_int = {}
     item_id_dict_counter = 1
 
     def __init__(self, file_name):
         self.file_name = file_name
-        self.writer = open("user_ratings_int_uid_int_itemid_full.csv", "wb")
-
+        self.writer = open("id_title_int_itemid_str_title_full.csv", "wb")
 
     def parse_item_block(self, data):
+
+        asin = ""
+        title = ""
         for item in data: #traverse list
             #clear item properties based on user input
             if 'ASIN:' in item:
-                # asin = item.partition(": ")[2].replace("\n", "")
                 asin = item.partition(": ")[2].rstrip()
-
                 if asin in self.item_id_to_int:
                     pass
                 else:
                     self.item_id_to_int[asin] = self.item_id_dict_counter
-                    self.item_id_dict_counter += 1
+                    self.item_id_dict_counter += 1           
 
             elif 'salesrank' in item:
                 sales_rank = item.partition(": ")[2].rstrip()
             elif 'avg rating' in item:
                 avg_rating = re.search(r'avg rating: [0-9]?.[0-9]?', item).group().partition(": ")[2].rstrip()
-            # elif 'categories' in item:
-            #     categories = 
             elif 'title:' in item:
                 title = item.partition(": ")[2].replace(",", " ").rstrip()
             elif 'group:' in item:
@@ -52,36 +50,30 @@ class UserIdList:
                     similar_list = 0
             elif 'cutomer:' in item:
                 temp_list = item.split()
-                # print(temp_list)
                 date = temp_list[0]
                 # date = re.search(r'[0-9]{,4}-[0-9]{1,2}-[0-9]{1,2}', item).group()
                 customer_id = temp_list[2]
                 user_rating = temp_list[4]
                 vote = temp_list[6]
                 helpful = temp_list[8].rstrip()
-                if customer_id in self.user_id_to_int:
-                    pass
-                else:
-                    self.user_id_to_int[customer_id] = self.user_id_dict_counter
-                    self.user_id_dict_counter += 1
-
                 # short print version
-                entry = "{0}, {1}, {2}".format(self.user_id_to_int[customer_id], self.item_id_to_int[asin], user_rating)                
-                # entry = "{0}, {1}, {2}".format(customer_id, asin, user_rating)
-                print(entry)
+                # print("{0}, {1}, {2}".format(customer_id, asin, user_rating))
 
                 # index: property
                 # 0: customer id, 1: amazon product id, 2: user rating, 3: votes, 4: helpful,
                 # 5: date, 6: sales rank, 7: title, 8: avg rating, 9: group, 10: similar item#
                 # 11: similar item IDs (spae delimited)
-
-
                 # entry = "{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}".format(
-                #     user_id_to_int[customer_id], asin, user_rating, vote, helpful, date, sales_rank, title, avg_rating,
+                #     customer_id, asin, user_rating, vote, helpful, date, sales_rank, title, avg_rating,
                 #     group, similar_count, similar_list)
                 # print(entry)
 
-                self.writer.write(entry + "\n")
+                # self.writer.write(entry + "\n")
+
+        if asin != "" and title != "":
+            entry = "{0}, {1}".format(self.item_id_to_int[asin], title)
+            print(entry)
+            self.writer.write(entry + "\n")
     '''
     'ASIN: 0738700797\n', '  title: Candlemas: Feast of Flames\n', '  group: Book\n', '  
     salesrank: 168596\n', '  similar: 5  0738700827  1567184960  1567182836  0738700525  
@@ -120,7 +112,7 @@ class UserIdList:
         return False
 
 if __name__=="__main__":
-    user_id_list = UserIdList("amazon-meta.txt")
+    user_id_list = ItemsMap("amazon-meta.txt")
     user_id_list.read_file()
 
 
